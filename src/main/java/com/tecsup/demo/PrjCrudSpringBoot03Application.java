5e57ad7b3e0c21fb9fdeaf2923e4a3a4683c325d
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2025-06-04 16:14:55 -0500

    Hecho

---
+++ src/main/java/com/tecsup/demo/PrjCrudSpringBoot03Application.java
@@ -0,0 +1,13 @@
+package com.tecsup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjCrudSpringBoot03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjCrudSpringBoot03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tecsup/demo/PrjCrudSpringBoot03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjCrudSpringBoot03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
